e775cc8cf9c9bb7e5758e9b1e3f09de0e37caee0
Mulori <murilogarcia.hr@gmail.com>
2023-03-11 14:19:59 -0300

    update

--- src/controller/api/index.jsx
+++ src/controller/api/index.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 
+/*
 const api = axios.create({
   baseURL: "https://vantagem-backend-r48db.ondigitalocean.app",
 });
+*/
 
-/*
 const api = axios.create({
   baseURL: "http://localhost:2258",
 });
-*/
+
 
 export default api;
